99cca7b36baed9d16ae229d238535a8df33ddac5
Sudi Williams <107420753+sudi-W@users.noreply.github.com>
2023-11-14 11:03:12 -0300

    Commit initial files

---
+++ README.md
@@ -0,0 +1,45 @@
+# SpeakOutSpace - Share Your Thoughts
+
+Welcome to SpeakOutSpace, an app for expressing your thoughts freely!
+
+## Overview
+
+This project is a simple web application where people can share their thoughts.
+
+## Technologies Used
+
+- HTML
+- CSS
+- JavaScript
+- Firebase Realtime Database
+
+## Getting Started
+
+1. Clone this repository.
+2. Open the `index.html` file in your preferred web browser.
+
+## Features
+
+- Users can write and publish comments.
+- Comments include "To," "Comment," and "From" fields.
+- Realtime updates using Firebase.
+
+## Firebase Integration
+
+This project uses Firebase for real-time data synchronization. The Firebase SDK is imported directly from the CDN.
+
+## How to Use
+
+1. Fill in the "Comment," "From," and "To" fields.
+2. Click the "Publish" button to submit your comment.
+3. Comments will be displayed in real-time below the input section.
+4. Click on a comment to remove it.
+
+## Manifest File
+
+The site.webmanifest file contains configuration settings for the web app, such as icons and theme colors.
+
+## Contributions
+
+Contributions are welcome! If you'd like to enhance the app's functionality or fix issues, feel free to fork the repository and submit a pull request.
+
---
+++ index.js
@@ -0,0 +1,104 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+
+const appSettings = {
+    projectId: "speakoutspace-38617",
+    dataBaseURL: "https://speakoutspace-38617-default-rtdb.firebaseio.com/"
+};
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const commentsListInDB = ref(database, "commentsList");
+
+const commentFieldEl = document.getElementById("comment-field");
+const fromFieldEl = document.getElementById("from-field");
+const toFieldEl = document.getElementById("to-field");
+const addButtonEl = document.getElementById("add-button");
+const commentSectionEl = document.getElementById("comments");
+
+
+addButtonEl.addEventListener("click", function() {
+
+    if (commentFieldEl.value.length === 0 || fromFieldEl.value.length === 0 || toFieldEl.value.length === 0) {
+        alert("Please fill in all fields before publishing.");
+        return;
+    }
+    
+    let inputValues = {
+        comment: commentFieldEl.value,
+        from: fromFieldEl.value,
+        to: toFieldEl.value
+    };
+
+    push(commentsListInDB, inputValues)
+
+    clearInputFields();
+    
+});
+
+onValue(commentsListInDB, function(snapshot) {
+
+    if (snapshot.exists()) {
+        
+        let itemsArray = Object.entries(snapshot.val())
+
+        clearcommentSectionEl();
+    
+        for (let i = 0; i < itemsArray.length; i++) {
+            let currentComment = itemsArray[i];
+            let currentCommentID = currentComment[0];
+            let currentCommentValue = currentComment[1];
+    
+            appendItemToCommentSectionEl(currentComment)
+    
+        }
+    } else {
+        commentSectionEl.innerHTML = "<span style='color: red;'>There aren't any items here just yet.</span>";
+    }
+    
+})
+
+function clearcommentSectionEl() {
+    commentSectionEl.innerHTML = "";
+}
+
+function clearInputFields() {
+
+    commentFieldEl.value = "";
+    fromFieldEl.value = "";
+    toFieldEl.value = "";
+
+}
+
+function appendItemToCommentSectionEl(comments) {
+
+    let commentsID = comments[0];
+    let commentsValue = comments[1];
+
+    // Create a new list item 
+    let listItem = document.createElement("li");
+
+    // Create separate paragraphs for "To", "Comment", and "From"
+    let toParagraph = document.createElement("p");
+    toParagraph.textContent = `To ${commentsValue.to}`;
+
+    let commentParagraph = document.createElement("p");
+    commentParagraph.textContent = commentsValue.comment;
+
+    let fromParagraph = document.createElement("p");
+    fromParagraph.textContent = `From ${commentsValue.from}`;
+
+    // Append paragraphs to the list item
+    listItem.appendChild(toParagraph);
+    listItem.appendChild(commentParagraph);
+    listItem.appendChild(fromParagraph);
+
+    commentSectionEl.appendChild(listItem); 
+
+    listItem.addEventListener("click", function() {
+        let exactLocationOfItemInDB = ref(database, `commentsList/${commentsID}`);
+        
+        remove(exactLocationOfItemInDB)
+    });
+
+}
